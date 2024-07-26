c166adefd7ede6a2f967d5f375cb0bea90cb3768
choijinsung <bugslife102401@nate.com>
2024-07-26 21:34:07 +0900

    Add d-day block

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const remainHour = document.querySelector('#d-day_hour');
 const remainMin = document.querySelector('#d-day_min');
 const remainSec = document.querySelector('#d-day_sec');
 
+const remainDay = document.querySelector('#remain-day');
+const remainOrPast = document.querySelector('#remain_or_past');
+
 function diffDay() {
   const weddingTime = new Date("2024-11-23").setHours(12, 30, 0, 0);
   const todayTime = new Date();
@@ -29,5 +32,25 @@ function diffDay() {
   remainSec.innerText = `${diffSec}`;
 }
 
+function calRemainDay() {
+  const weddingDay = new Date("2024-11-23").setHours(0, 0, 0, 0);
+  const today = new Date().setHours(0, 0, 0, 0);
+
+  const diff = weddingDay - today;
+  let diffDays;
+
+  if (diff < 0) {
+    diffDays = diff*(-1) / (1000*60*60*24);
+    remainOrPast.innerText = '지났습니다.';
+  } else {
+    diffDays = diff / (1000*60*60*24);
+  }
+
+  remainDay.innerText = `${diffDays}`;
+}
+
 diffDay();
 setInterval(diffDay, 1000);
+
+calRemainDay();
+setInterval(calRemainDay, 1000*60*60*24);
