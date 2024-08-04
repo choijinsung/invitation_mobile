9217ff32a8fc11d4026c55761f20b1e56710b2f0
choijinsung <bugslife102401@nate.com>
2024-08-04 09:10:49 +0900

    Move loading api source

--- script.js
+++ script.js
@@ -84,12 +84,3 @@ setInterval(calRemainDay, 1000*60*60*24);
 // });
 
 // b75e7c0e521976aa6642a7c905fb48d8
-
-let container = document.getElementById('map');
-
-let options = {
-  center: new kakao.maps.LatLng(33.450701, 126.570667),
-  level: 3
-};
-
-let map = new kakao.maps.Map(container, options);
