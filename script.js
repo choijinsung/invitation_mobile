6f4e654e31ced4f0599508991d71c702b325f5cf
최진성 <schoi@raon-tech.com>
2024-09-12 15:45:00 +0900

    Add new images

--- script.js
+++ script.js
@@ -157,19 +157,4 @@ setInterval(diffDay, 1000);
 calRemainDay();
 setInterval(calRemainDay, 1000*60*60*24);
 
-// let mapButton = document.querySelector('.location_map_button');
-// let dynamicMap = document.querySelector('.location_dynamic_map');
-// let staticMap = document.querySelector('.location_static_map');
-
-// mapButton.addEventListener('click', function() {
-//   if(mapButton.textContent === '약도보기') {
-//     mapButton.innerText = '지도보기';
-//   } else if(mapButton.textContent === '지도보기') {
-//     mapButton.innerText = '약도보기';
-//   }
-
-//   dynamicMap.classList.toggle('show');
-//   staticMap.classList.toggle('show');
-// });
-
 // b75e7c0e521976aa6642a7c905fb48d8
